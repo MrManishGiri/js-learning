25fd2a22b8496273bbc9d3f3f43d7ae85b7f9ad9
MrManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-10-31 09:01:07 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -19,7 +19,50 @@
 // "something" => true "" => false
 
 
-let someNumber = 33;
-let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let someNumber = 33;
+// let StringNumber = String(someNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// *************** operations ***************
+
+let value= 7;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(3 + 3);
+// console.log(3 - 3);
+// console.log(3 * 3);
+// console.log(3 / 3);
+// console.log(3 % 3);
+// console.log(3 ** 3);
+
+let str1 = "Hello";
+let str2 = " World";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(3 + "3");
+// console.log(3 - "3");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log(+true); this should not be done in real life.
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 10;
+console.log(num3);
+
+
+let gameCounter = 100;
+gameCounter++ ;
+console.log(gameCounter);   
+++gameCounter;
+console.log(gameCounter);
+
+
+// link to study https://tc39.es/ecma262/multipage/ecmascript-language-expressions.html#sec-postfix-increment-operator
