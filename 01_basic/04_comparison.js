2ec01a374ccf3fae45a502a3734db309b0f10a05
MrManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-10-31 11:18:37 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log("2">1);
+// console.log("01">=1);
+// console.log("a">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+
+// === it checks the dataType and value
+console.log("2"===2);
+console.log("2"!==2);
+
+// == it checks only value
