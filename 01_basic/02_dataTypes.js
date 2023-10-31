1b046c100e05120b8d3ee74de96aa69ace64dbc3
MrManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-10-31 08:00:15 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // use strict mode treat all JS code as a new version of JS
+
+// alert('Hello World!');
+// alert(3 + 3); // we are using node.js, not browser. so we can't see alert. but we can see console.log
+ console.log(3 
+    + 3); // code readability should be high. so we can use enter to make it readable.
+
+    console.log("Manish");
+
+    let name="Manish";
+    let age=20;
+    let isLoggedin=false;
+    let state = null;
+
+
+    // number => 2^53 -1
+    //bigint => 2^53 -1
+    //String => " "
+    //boolean => true/false
+    // null => null is a standalone type that has a single value null.
+    //undefined => undefined is a type itself (undefined) with a single value undefined.
+    //symbol => Symbol is a primitive type for unique identifiers.
+    //object => object is a type that represents the non-primitive type, i.e. any thing that is not number, bigint, string, boolean, null, or undefined.
+
+    console.log(typeof "name");
+    console.log(typeof null);
+    console.log(typeof undefined);
+
