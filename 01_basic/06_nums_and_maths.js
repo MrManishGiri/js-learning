dba3082b077f3d6ec736badf005fbf16b519756b
Mr.ManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-11-01 14:30:57 +0545

    Numbers and Maths

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,48 @@
+// const balance = new Number(100);
+// console.log(balance);
+
+// const score = 400;
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+// console.log(balance);
+
+// console.log(balance.toFixed(2));    // 100.00
+
+// const otherNumber = 23.4325;
+// console.log(otherNumber);
+
+// console.log(otherNumber.toPrecision(3));    // 23.4 precision is the number of digits to display in total 
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN')); // localeString is used to display the number in the format of the country
+
+
+
+// ****************** Math ******************************
+
+// console.log(Math);
+
+// console.log(Math.abs(-100));    // 100
+
+// console.log(Math.PI);   // 3.141592653589793    
+
+// console.log(Math.round(2.4));   // 2
+
+// console.log(Math.ceil(2.4));    // ceiling value is 3
+// console.log(Math.floor(2.4));   // floor value is 2
+
+// console.log(Math.sqrt(64)); // 8
+
+// console.log(Math.pow(2, 3));    // 8
+
+// console.log(Math.min(2, 3, 4, 5, 6, 7, 8, 9, 10));    // 2
+// console.log(Math.max(2, 3, 4, 5, 6, 7, 8, 9, 10));    // 10
+
+console.log(Math.random( )); // random number between 0 and 1
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min) + min)); // random number between 10 and 20
