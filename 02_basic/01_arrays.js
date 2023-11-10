545298d42c7008b78a479e354411c72ddcc1e4ff
Mr.ManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-11-10 21:30:35 +0545

    array

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,23 @@
+// Array
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const myHeros =["IronMan","ShaktiMan"]
+
+// console.log(myArray);
+// console.log(myHeros)
+
+// Arrays Methods
+
+// myArray.push("Manish")
+// console.log(myArray)
+
+// myArray.pop()   //remove last value
+
+
+// myArray.unshift("Manish") // it will put the value at the first index of 0th index.
+// myArray.shift()
+
+console.log(myArray.includes("Manish"))
+console.log(myArray.indexOf("Manish"));
+console.log(myArray)
