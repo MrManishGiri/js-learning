22c746ee7063060ae559e2b9c2db43648c3bd33a
Mr.ManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-11-12 00:19:16 +0545

    half done

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -2,7 +2,7 @@
 
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const myHeros =["IronMan","ShaktiMan"]
+// const myHeros =["IronMan","ShaktiMan"]
 
 // console.log(myArray);
 // console.log(myHeros)
@@ -18,6 +18,26 @@ const myHeros =["IronMan","ShaktiMan"]
 // myArray.unshift("Manish") // it will put the value at the first index of 0th index.
 // myArray.shift()
 
-console.log(myArray.includes("Manish"))
-console.log(myArray.indexOf("Manish"));
-console.log(myArray)
+// console.log(myArray.includes("Manish"))
+// console.log(myArray.indexOf("Manish"));
+// console.log(myArray)
+
+// const newArr = myArray.join()
+
+//  console.log(myArray);
+//  console.log(typeof newArr);
+  
+//  console.log(newArr)
+
+// Slice, Splice
+
+// console.log("A", myArray);
+
+// const myn1 =myArray.slice (1,3)
+
+// console.log(myn1)
+// console.log("B", myArray)
+
+// const myn2 = myArray.splice(1,3)
+// console.log("C", myArray)
+// console.log(myn2)
