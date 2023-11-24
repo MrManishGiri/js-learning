7a880784d1c3d36cba6e440fa09f5cc53ae66071
Mr.ManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-11-24 22:43:54 +0545

    Destructuring and JSON

---
+++ 02_basic/05_objects.js
@@ -0,0 +1,21 @@
+// object destructuring
+
+const coursesJS ={
+    coursename: "js in nepali",
+    price : "1699",
+    instructor :"Manish"
+}
+
+const {coursename:course1} =coursesJS
+
+console.log(course1);
+
+// React destructuring 
+
+// const name = ({company}) => {
+
+// }
+
+// name(company="Manईस")
+
+
