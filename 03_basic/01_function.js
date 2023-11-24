9a87b17eb9bac8324c158c4f588a3b3805bdcaf7
Mr.ManishGiri <138374299+MrManishGiri@users.noreply.github.com>
2023-11-25 01:04:32 +0545

    function done.

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -43,5 +43,37 @@ function name(username="Mr.Giri") {
     }
     return `${username} just shown in the website.` 
 }
-console.log(name());
-console.log(name("Manish"));
+// console.log(name());
+// console.log(name("Manish"));
+
+function calculateCartPrice (...alpha /* rest operator */) {
+//rest operator is used to represent an indefinite number of arguments as an array.
+    return alpha
+}
+// console.log(calculateCartPrice(100,300,500 /*arguments*/));
+
+
+function calcPrice(price1, price2, ...paid) {
+    return paid
+}
+// console.log(calcPrice(50,80,90,100,250)); // output: [ 90, 100, 250 ]
+
+// const user = {
+//     username:"Manish",
+//     age:21
+// }
+
+function hancy(anyObj) {
+    // console.log(`username is ${anyObj.username} and age is ${anyObj.age}`)
+}
+// hancy(user)
+hancy({
+    username:"Manish",
+    age:22
+})
+
+const priceArray = [200,300,50,100]
+function return2ndPrice(gotArray) {
+    return gotArray[3]
+}
+console.log(return2ndPrice(priceArray));
